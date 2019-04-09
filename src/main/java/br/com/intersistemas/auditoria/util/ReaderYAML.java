e18ba9d509254534b0f944ce63fd41bc13b5349a
Suporte <Suporte@SUPORTE>
2019-04-09 17:16:47 -0300

    Nova leitura da configuração do .yaml

--- src/main/java/br/com/intersistemas/auditoria/util/ReaderYAML.java
+++ src/main/java/br/com/intersistemas/auditoria/util/ReaderYAML.java
@@ -8,6 +8,8 @@ package br.com.intersistemas.auditoria.util;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import org.yaml.snakeyaml.Yaml;
 import org.yaml.snakeyaml.constructor.Constructor;
 
@@ -17,21 +19,30 @@ import org.yaml.snakeyaml.constructor.Constructor;
  */
 public class ReaderYAML {
 
-    public static ConfigDataBase read() throws FileNotFoundException {
-        Yaml yaml = new Yaml(new Constructor(ConfigDataBase.class));
+    public static ConfigMysqlConnection read() throws FileNotFoundException {
+        Yaml yaml = new Yaml(new Constructor(ConfigMysqlConnection.class));
         File file = new File("database.yaml");
 
         if (!file.exists()) {
             throw new FileNotFoundException("O arquivo " + file.getAbsolutePath() + " não existe.");
         }
 //        log.append(file.getAbsolutePath() + "\n");
-        ConfigDataBase configDatabase = yaml.load(new FileReader(file));
+        ConfigMysqlConnection configMysqlConnection = yaml.load(new FileReader(file));
 //        log.append(configDatabase.toString() + "\n");
 
-        if (configDatabase == null) {
+        if (configMysqlConnection == null) {
             throw new FileNotFoundException("O arquivo YAML está com estrutura inválida");
         }
 
-        return configDatabase;
+        return configMysqlConnection;
     }
+    
+   /* public static void main(String[] args) {
+        try {
+            ConfigMysqlConnection configMysqlConnection = ReaderYAML.read();
+            System.out.println(configMysqlConnection.toString());
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ReaderYAML.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }*/
 }
