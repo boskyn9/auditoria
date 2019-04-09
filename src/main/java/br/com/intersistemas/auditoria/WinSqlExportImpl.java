038d1624ac34400afe4f53c187a3102e10592662
Suporte <Suporte@SUPORTE>
2019-04-09 17:18:23 -0300

    Nova leitura da configuração do .yaml

--- src/main/java/br/com/intersistemas/auditoria/WinSqlExportImpl.java
+++ src/main/java/br/com/intersistemas/auditoria/WinSqlExportImpl.java
@@ -6,12 +6,15 @@
 package br.com.intersistemas.auditoria;
 
 import br.com.intersistemas.auditoria.util.ConfigDataBase;
+import br.com.intersistemas.auditoria.util.ConfigMysqlConnection;
+import br.com.intersistemas.auditoria.util.DataBase;
 import br.com.intersistemas.auditoria.util.ReaderYAML;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.logging.Level;
@@ -29,33 +32,45 @@ public class WinSqlExportImpl implements SqlExport {
 
         try {
             String home = System.getProperty("user.home");
-            ConfigDataBase db = ReaderYAML.read();
+            // ANTES: ConfigDataBase db = ReaderYAML.read();
+            ConfigMysqlConnection dbReader = ReaderYAML.read();
+            List<ConfigDataBase> hosts = dbReader.getHosts();
+            //db.getDataBases().forEach(dataBase -> {
+//            for (int i = 0; i <; i++) { //**@brief 3 = Numero de banco de dados
+            hosts.forEach(hostConfig -> {
 
-            db.getDataBases().forEach(dataBase -> {
+                List<DataBase> dataBases = hostConfig.getDataBases();
 
-                try {
-                    String fileName = home + "\\auditoria_" + new Date().getTime() + ".html";
-                    File file = new File(fileName);
+                dataBases.forEach(db -> {
+                    try {
+                       // String nameFile = dbReader.getSql(params);
+                       // System.out.println("Name File: "+nameFile);
+                        String fileName = home + "\\auditoria_" +  +new Date().getTime() + ".html";
+                        File file = new File(fileName);
 
-                    String command = String.format("mysql -h %s -u %s -p%s -D %s -H -e \" %s \" > %s", db.getHost(), db.getUser(), db.getPassword(), dataBase.getName(), db.getSql(params), fileName);
+                        String command = String.format("mysql -h %s -u %s -p%s -D %s -H -e \" %s \" > %s", hostConfig.getHost(), hostConfig.getUser(),hostConfig.getPassword(), db.getName(), dbReader.getSql(params), fileName);
 
-                    System.out.println(command);
+                        System.out.println(command);
 
-                    ProcessBuilder processBuilder = new ProcessBuilder();
-                    processBuilder.command("cmd.exe", "/C", command); 
-                   // processBuilder.command(command);
-                    Process process = processBuilder.start();
-                    process.waitFor();
+                        ProcessBuilder processBuilder = new ProcessBuilder();
+                        processBuilder.command("cmd.exe", "/C", command);
+                        //processBuilder.command(command);
+                        Process process = processBuilder.start();
+                        process.waitFor();
+                        System.out.println("Ponto após executar o mysql");
+                        System.out.println("Tamanho do arquivo:" + file.length());
+                        if (file.length() > 0) {
+                            System.out.println("Adiciona na Lista de arquivos");
+                            files.add(file);
+                        } else {
+                            file.delete();
+                            System.out.println("Deleta o arquivo");
+                        }
 
-                    if (file.length() > 0) {
-                        files.add(file);
-                    } else {
-                        file.delete();
+                    } catch (IOException | InterruptedException ex) {
+                        Logger.getLogger(LinuxSqlExportImpl.class.getName()).log(Level.SEVERE, null, ex);
                     }
-
-                } catch (IOException | InterruptedException ex) {
-                    Logger.getLogger(LinuxSqlExportImpl.class.getName()).log(Level.SEVERE, null, ex);
-                }
+                });
 
             });
 
